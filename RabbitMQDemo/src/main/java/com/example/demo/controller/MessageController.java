7422d4c11274641d17e48f710c3b9b86aa879aef
sakshimote <sakshimote19@gmail.com>
2023-11-28 19:38:09 +0530

    rabbit mq added

---
+++ RabbitMQDemo/src/main/java/com/example/demo/controller/MessageController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.publisher.RabbitMQProducer;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageController {
+	
+	
+	private RabbitMQProducer producer;
+	
+	
+	public MessageController(RabbitMQProducer producer) {
+		this.producer=producer;
+		
+	}
+	
+	@GetMapping("/publish")
+	public ResponseEntity<String> sendMessage(@RequestParam("message") String message){
+		producer.sendMessage(message);
+		return ResponseEntity.ok("mesaage sent to rabbitmq..............");
+		
+		
+	}
+	
+	
+
+}
