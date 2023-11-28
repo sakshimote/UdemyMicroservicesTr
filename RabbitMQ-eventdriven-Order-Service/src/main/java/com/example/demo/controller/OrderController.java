af28d89833c5f0be4e4eea2d7d4ab2be0cff0f52
sakshimote <sakshimote19@gmail.com>
2023-11-28 22:52:54 +0530

    latest all done for rabbit mq

---
+++ RabbitMQ-eventdriven-Order-Service/src/main/java/com/example/demo/controller/OrderController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.Order;
+import com.example.demo.dto.OrderEvent;
+
+import com.example.demo.publisher.OrderProducer;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+    private OrderProducer orderProducer;
+
+    public OrderController(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
+    @PostMapping("/orders")
+    public String placeOrder(@RequestBody Order order){
+
+        order.setOrderId(UUID.randomUUID().toString());
+
+        OrderEvent event = new OrderEvent();
+        event.setStatus("PENDING");
+        event.setMessage("Order is in pending status");
+        event.setOrder(order);
+
+        orderProducer.sendMessage(event);
+
+        return "Order sent to the RabbitMQ ..";
+    }
+}
